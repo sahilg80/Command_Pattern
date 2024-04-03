64003e31bead7650c089f1c2dadaa3c8a38dfa13
sahilg80 <38989332+sahilg80@users.noreply.github.com>
2024-04-03 19:05:04 +0530

    Added command pattern

---
+++ Assets/Scripts/Commands/UnitCommand.cs
@@ -0,0 +1,32 @@
+﻿using Command.Player;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.Commands
+{
+    public abstract class UnitCommand : ICommand
+    {
+        public int ActorUnitID;
+        public int TargetUnitID;
+        public int ActorPlayerID;
+        public int TargetPlayerID;
+
+        // References to the actor and target units, accessible by subclasses.
+        protected UnitController actorUnit;
+        protected UnitController targetUnit;
+
+        /// <summary>
+        /// Abstract method to execute the unit command. Must be implemented by concrete subclasses.
+        /// </summary>
+        public abstract void Execute();
+
+        /// <summary>
+        /// Abstract method to determine whether the command will successfully hit its target.
+        /// Must be implemented by concrete subclasses.
+        /// </summary>
+        public abstract bool WillHitTarget();
+    }
+}
